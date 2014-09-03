f36dee759fc85b0960e8b918a52136c87ea3620c
Mitchell Lane <mitchell@nightsprout.com>
2014-09-03 17:14:26 -0400

    adding in puma

---
+++ config/puma.rb
@@ -0,0 +1,18 @@
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+threads Integer(1), Integer(1) # Not threadsafe currently
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # worker specific setup
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
