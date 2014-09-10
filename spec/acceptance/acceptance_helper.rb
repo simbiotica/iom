217c846af23c54f9f48db4f1f18dee6480c24977
dtpowl <dtpowl@gmail.com>
2014-09-10 11:07:50 -0400

    acceptance spec config

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -4,7 +4,7 @@ require 'capybara/rails'
 require "capybara/dsl"
 require "selenium-webdriver"
 
-Capybara.default_driver    = :rack_test
+Capybara.default_driver    = :selenium
 Capybara.default_host      = 'example.com'
 Capybara.server_port       = 9887
 Capybara.app_host          = "http://#{Capybara.default_host}:#{Capybara.server_port}"
@@ -31,19 +31,19 @@ RSpec.configure do |config|
     #    stub(u).delete_attached_files {true}
     #  end
     any_instance_of(Paperclip::Attachment, :post_process => true)
-    Capybara.current_driver = :selenium
+    Capybara.current_driver = :selenium if example.metadata[:js]
   end
 
   config.after(:each) do
     case page.driver.class
     when Capybara::Driver::RackTest
       page.driver.rack_mock_session.clear_cookies
-    when Capybara::Driver::Culerity
-      page.driver.browser.clear_cookies
+#    when Capybara::Driver::Culerity
+#      page.driver.browser.clear_cookies
     when Capybara::Driver::Selenium
       page.driver.cleanup!
     end
-    Capybara.use_default_driver
+    Capybara.use_default_driver if example.metadata[:js]
   end
 
 end
